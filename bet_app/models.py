fe78895b9483657ee825ec6797d0fd31a896b750
steph-prod <footstream2023@gmail.com>
2024-02-20 13:52:10 +0300

    🎉 Build Model and some views for the project.

---
+++ bet_app/models.py
@@ -0,0 +1,25 @@
+# Create your models here.
+from django.db import models
+
+
+# BET Model where a user will use his phone number
+
+class Bet(models.Model):
+    phone_number = models.CharField(max_length=15)
+    reference = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Bet by {self.phone_number} for {self.amount}'
+
+
+# TRANSACTION Model to help a user make his transaction
+class Transaction(models.Model):
+    bet = models.ForeignKey(Bet, on_delete=models.CASCADE)
+    transaction_type = models.CharField(
+        max_length=10, choices=[('deposit', 'Deposit'), ('withdraw', 'Withdraw')])
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.transaction_type} of {self.amount} for bet {self.bet.id}'
