869985316d9cf5e1b656b524ab3dc9e11cff08b2
steph-prod <footstream2023@gmail.com>
2024-02-21 10:50:27 +0300

    add mpesa config

--- backend/urls.py
+++ backend/urls.py
@@ -17,17 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from mpesa.urls import mpesa_urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('bet_app.urls'))
+    path("admin/", admin.site.urls),
+    path("", include("bet_app.urls")),
+    path("mpesa/", include(mpesa_urls)),
 ]
 
 
 #  👉 to add in urls
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
